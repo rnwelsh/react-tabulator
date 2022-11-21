e691797986f31d53bf1744ec420da3552f0ad6f5
rnwelsh <rnwelsh@ualr.edu>
2022-11-21 12:00:31 -0600

    dont use experimental fs.cp, prepare for npm

--- copyStyles.mjs
+++ copyStyles.mjs
@@ -1,32 +1,36 @@
-import fs from 'fs'
-import {join} from 'node:path'
+import {mkdirSync, readdir, copyFileSync} from 'node:fs'
+import {join, extname} from 'node:path'
 
 
 /**
- * use node's experimental fs.cpSync module for cross-platform support
+ * use fs for cross-platform support
  */
 function copyStyles() {
-  fs.mkdirSync(join('./','lib','css'),{recursive:true})
+  mkdirSync(join('./','lib','css'),{recursive:true})
   
-  // copy css files from tabulator-tables
-  const sourceDirName = join('node_modules','tabulator-tables','dist','css')
-  const destDirName = join('lib','css')
+  // copy css files directly from node_modules/tabulator-tables
   
-  // experimental cp (and cpSync) don't support relative paths
   const cwd = process.cwd()
+
+  const sourcePath    = join(cwd, 'node_modules','tabulator-tables','dist','css')
+  const destBasePath  = join(cwd, 'lib')
+  const destCssDirPath= join(cwd, 'lib','css')
   
-  const sourcePath= join(cwd, sourceDirName)
-  const destBasePath = join(cwd, 'lib')
-  const destPath = join(cwd, destDirName)
-  
-  // works like cp -R
-  fs.cpSync(sourcePath,destPath,{recursive: true})
+  readdir(sourcePath, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      // copy only css files
+      if(extname(file)==='.css') {
+        copyFileSync(join(sourcePath,file), join(destCssDirPath,file))
+      }
+    }
+  });
   
   const srcStylesFileName = 'styles.css'
   const srcStylesFilePath = join(cwd,'src',srcStylesFileName)
   
-  fs.copyFileSync(srcStylesFilePath, join(destBasePath,srcStylesFileName))
-  fs.copyFileSync(srcStylesFilePath, join(destPath,srcStylesFileName))
+  copyFileSync(srcStylesFilePath, join(destBasePath  ,srcStylesFileName))
+  copyFileSync(srcStylesFilePath, join(destCssDirPath,srcStylesFileName))
 }
 
 copyStyles()
